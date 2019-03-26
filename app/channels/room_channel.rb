d8c306b7092e3c757dbaf0873f16a900c4b32aef
Jose Luis Cambero <jlcambero@gmail.com>
2019-03-26 18:07:04 +0100

    Update list of users with action cable

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -3,10 +3,17 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_#{params[:room]}"
+
+    if params[:room].starts_with?('user_')
+      ActionCable.server.broadcast('room_userlist', 'update')
+    end
     # room_list
     # room_id
     # room_user_xxx
+    # room_userlist
   end
 
-  def unsubscribed; end
+  def unsubscribed
+    ActionCable.server.broadcast('room_userlist', 'update') if params[:room].starts_with?('user_')
+  end
 end
